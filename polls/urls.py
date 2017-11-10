8d1b0d6420f6c8af51fd5b74200b602caa4791e3
Jared Vicentillo <jared.vicentillo@gmail.com>
2017-11-10 18:00:18 +0800

    Added results

--- polls/urls.py
+++ polls/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^createquestion/$',
         views.CreateQuestion.as_view(), name='createquestion'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/result/$',
+        views.ResultView.as_view(), name='result'),
 ]
