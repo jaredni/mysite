65572066f10d35bbfbedb5be8a6c6a11bacdda02
Jared Vicentillo <jared.vicentillo@gmail.com>
2017-11-10 15:06:16 +0800

    Added IndexView class

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,15 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from polls.models import Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.all()
 
 # Create your views here.
