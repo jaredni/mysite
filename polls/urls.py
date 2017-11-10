a3d1386fc1ddf8a471a318f4efa16f6d0717d240
Jared Vicentillo <jared.vicentillo@gmail.com>
2017-11-10 15:19:15 +0800

    Added CreateQuestion to polls.urls

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^createquestion/$',
+        views.CreateQuestion.as_view(), name='createquestion'),
 ]
